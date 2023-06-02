1edf0db11a1b02296bc8355e7fc6077fbf9a68d7
Mithlesh kumar <110152369+AdarshSingh172@users.noreply.github.com>
2023-06-02 18:42:21 +0530

    Add files via upload

---
+++ src/data/BlogData.js
@@ -0,0 +1,44 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"The Scammers of the Blogging World",
+        tags:["Scammmers"],
+        date:"16 Feb, 2023",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/npxXWgQ33ZQ/upload/11cae5b5d6218b8ca9aa256d630a09c0.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://memithlesh73.hashnode.dev/the-scammers-of-the-blogging-world"
+    },
+    {
+        id:2,
+        name:"nullish coalescin & optional chaining",
+        tags:["javascript,developer"],
+        date:"01 June, 2023",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/f77Bh3inUpE/upload/42d7eae9d33ab0f0a231c37e6f1940cf.jpeg?w=1600&amp;h=840&amp;fit=crop&amp;crop=entropy&amp;auto=compress,format&format=webp",
+        link:"https://memithlesh73.hashnode.dev/nullish-coalescing-optional-chaining"
+    },
+    {
+        id:3,
+        name:"5G Network",
+        tags:['Automation', 'Operations & Testing'],
+        date:"02 June, 2023",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/YzZJUXjb9aw/upload/b31d74daa64007bdaeca369706d4a94a.jpeg?w=1600&amp;h=840&amp;fit=crop&amp;crop=entropy&amp;auto=compress,format&format=webp",
+        link:"https://memithlesh73.hashnode.dev/5g-network"
+    },
+    {
+        id:4,
+        name:"stock market",
+        tags:["stock"],
+        date:"1 June, 2022",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/mcAUHlGirVs/upload/ff664d94e9f2c753b4d16b45303de732.jpeg?w=1600&amp;h=840&amp;fit=crop&amp;crop=entropy&amp;auto=compress,format&format=webp",
+        link:"https://memithlesh73.hashnode.dev/stock-market"
+    },
+    {
+        id:5,
+        name:"ChatGPT",
+        tags:["AI technology"],
+        date:"02 June, 2023",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/-0xMiYQmk8g/upload/fe7f7185469c93d63965b7eb9ad48c02.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://memithlesh73.hashnode.dev/chatgpt"
+    },
+    
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,41 @@
+export const Work = [
+    {
+        id:1,
+        name:"Emart ",
+        description:"It is a e-commerce build on top of the React JS, with dummy.JSON as backend",
+        tags:["react","dummyjson"],
+        
+        demo:"https://emart-172.netlify.app/",
+        github:"https://github.com/AdarshSingh172/Ecommerce-Emart-App"
+    },
+    {
+        id:2,
+        name:"Extension",
+        description:"A chrome extension build in ReactJS. With connect with local storage",
+        tags:["react","mockbee"],
+        
+        demo:"https://pocket-chrome172.netlify.app/",
+        github:"https://github.com/AdarshSingh172/Pocket-Chrome-Extension"
+        
+    },
+    {
+        id:3,
+        name:"Service-App-UI",
+        description:"A UI build in ReactJS.",
+        tags:["react","UI/UX","design"],
+        
+        demo:"https://service-clone172.netlify.app",
+        github:"https://github.com/AdarshSingh172/service-clone"
+    },
+  {
+        id:4,
+        name:"Netflix-clone",
+        description:"Build by Netflix Clone Web-App with React, Firebase & Material-UI!.",
+        tags:["react","Material-UI"],
+        
+        demo:"https://netflix-clone172.netlify.app",
+        github:"https://github.com/AdarshSingh172/netflix-clone"
+        
+    },
+
+] 
